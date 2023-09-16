7a1ea1a4ed3ae2fd77a627a73e729cd816b7eb2f
kwontaehoon <gju04195@gmail.com>
2023-09-16 20:44:13 +0900

    디렉토리 구조화

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  const Test = <div>gg</div>
+  return (
+    <div>
+      <div>App</div>
+      <div><Test></Test></div>
+    </div>
+  )
+}
+
+export default App
