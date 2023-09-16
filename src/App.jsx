3e482a6ce6e70f3fd3f8db95c9e45628222b831f
kwontaehoon <gju04195@gmail.com>
2023-09-16 20:44:36 +0900

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
 import React from 'react'
 
 const App = () => {
-  const Test = <div>gg</div>
   return (
-    <div>
-      <div>App</div>
-      <div><Test></Test></div>
-    </div>
+    <div>App</div>
   )
 }
 
